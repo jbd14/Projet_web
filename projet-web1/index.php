01676bc6baa73010c107a465400cb0f531339bb7
victorien grude <spi2023@ic2s124-04.univ-lemans.fr>
2014-11-14 10:14:36 +0100

    TEST

---
+++ projet-web1/index.php
@@ -0,0 +1,48 @@
+
+<html>  	
+<head>
+ <title> Ludothèque le mans </title>
+ <meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="../projet-web/CSS/principale.css"/>
+</head>
+ 
+<body>
+ <header>
+	 
+ </header>
+ 
+<div id="menu">
+	<ul class="menu">
+			<li><a href="#">Home</a></li>
+			<li>
+					<a href="#">Categories</a>
+					<ul>
+							<li><a href="#">CSS</a></li>
+							<li><a href="#">Graphic design</a></li>
+							<li><a href="#">Development tools</a></li>
+							<li><a href="#">Web design</a></li>
+					</ul>
+			</li>
+			<li>
+				<a href="#">Work</a>
+				<ul>
+							<li><a href="#">CSS</a></li>
+							<li><a href="#">Graphic design</a></li>
+							<li><a href="#">Development tools</a></li>
+							<li><a href="#">Web design</a></li>
+					</ul>
+			</li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+	</ul>
+</div>
+ 
+  <div id="corp">
+ </div>
+ 
+ <footer>
+ </footer>
+</body>
+
+</html>
+  
